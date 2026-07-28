62df010ead41dfd7c58258f45eccee9d8ef0fb69
naxty <9141879+naxty@users.noreply.github.com>
2026-07-28 17:16:44 +0200

    feat(gcp): add EssentialContacts::Contact

--- pkg/resources/cfres.go
+++ pkg/resources/cfres.go
@@ -13,6 +13,7 @@ import (
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/compute"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/container"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/dns"
+	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/essentialcontacts"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/gkehub"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/iam"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/pubsub"
