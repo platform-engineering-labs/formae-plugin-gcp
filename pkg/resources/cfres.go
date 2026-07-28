7e12a8926a15c1a95c426bcdc31fdb5bbebc0ab1
naxty <9141879+naxty@users.noreply.github.com>
2026-07-28 17:16:34 +0200

    feat(gcp): add GKEBackup::BackupPlan

--- pkg/resources/cfres.go
+++ pkg/resources/cfres.go
@@ -13,6 +13,7 @@ import (
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/compute"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/container"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/dns"
+	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/gkebackup"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/gkehub"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/iam"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/pubsub"
