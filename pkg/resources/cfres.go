612728ae499e91f8003957b52f1338ba7412c58b
naxty <9141879+naxty@users.noreply.github.com>
2026-07-28 12:22:34 +0200

    feat(gcp): add Redis::Instance

--- pkg/resources/cfres.go
+++ pkg/resources/cfres.go
@@ -16,6 +16,7 @@ import (
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/gkehub"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/iam"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/pubsub"
+	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/redis"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/secretmanager"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/servicenetworking"
 	_ "github.com/platform-engineering-labs/formae-plugin-gcp/pkg/resources/sql"
